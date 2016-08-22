7d03f9391fcdd7071a76b04ea5f87748fbd8dd84
yoku0825 <yoku0825@gmail.com>
2016-08-22 16:07:17 +0900

    Fix table logging (only logging when logging.level= DEBUG)
    * Re-fixed mikasafabric for MySQL 0.0.3's implementation
      * b3262b05f87524f3e50a8772590dcca5912787a1

--- mysql/fabric/handler.py
+++ mysql/fabric/handler.py
@@ -303,8 +303,7 @@ class MySQLHandler(logging.Handler, _persistence.Persistable):
             except AttributeError:
                 info_reported = get_time_from_timestamp(record.created)
 
-            ### TODO: Handle it by config
-            if False:
+            if _LOGGER.getEffectiveLevel() == 10: ### debug.level
                 MySQLHandler.add(
                     info_subject, info_reported, info_reporter,
                     self.format(record), info_category, info_type
